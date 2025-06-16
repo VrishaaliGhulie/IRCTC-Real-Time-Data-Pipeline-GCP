898b3f4a64cfd44ac3151c5b080a554fcc9a94a2
Vrishaali Ghulie <79352747+VrishaaliGhulie@users.noreply.github.com>
2025-06-16 13:31:14 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+PROJECT_ID = "gds-project-432013" 
+TOPIC_ID = "irctc-data"  
+SUBSCRIPTION_ID = "irctc-data-sub"  
+BQ_DATASET = "irctc_dwh" 
+BQ_TABLE = "irctc_stream_tb"  
+# TEMP_LOCATION = "gs://your-bucket/temp"  
+# STAGING_LOCATION = "gs://your-bucket/staging"  
